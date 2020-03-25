e8495a6b94a5af73f263655a194ed716295edba9
AMobOfHamsters <d.tyler.senkbeil@gmail.com>
2020-03-25 13:21:26 -0400

    EditForm creation

---
+++ Database/Database/EditForm.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Database
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title_Label = new System.Windows.Forms.Label();
+            this.create_button = new System.Windows.Forms.RadioButton();
+            this.read_Button = new System.Windows.Forms.RadioButton();
+            this.update_Button = new System.Windows.Forms.RadioButton();
+            this.delete_Button = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // title_Label
+            // 
+            this.title_Label.AutoSize = true;
+            this.title_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_Label.Location = new System.Drawing.Point(231, 9);
+            this.title_Label.Name = "title_Label";
+            this.title_Label.Size = new System.Drawing.Size(321, 37);
+            this.title_Label.TabIndex = 2;
+            this.title_Label.Text = "Database Edit Form";
+            // 
+            // create_button
+            // 
+            this.create_button.AutoSize = true;
+            this.create_button.Location = new System.Drawing.Point(38, 61);
+            this.create_button.Name = "create_button";
+            this.create_button.Size = new System.Drawing.Size(56, 17);
+            this.create_button.TabIndex = 3;
+            this.create_button.TabStop = true;
+            this.create_button.Text = "Create";
+            this.create_button.UseVisualStyleBackColor = true;
+            // 
+            // read_Button
+            // 
+            this.read_Button.AutoSize = true;
+            this.read_Button.Location = new System.Drawing.Point(38, 84);
+            this.read_Button.Name = "read_Button";
+            this.read_Button.Size = new System.Drawing.Size(51, 17);
+            this.read_Button.TabIndex = 4;
+            this.read_Button.TabStop = true;
+            this.read_Button.Text = "Read";
+            this.read_Button.UseVisualStyleBackColor = true;
+            // 
+            // update_Button
+            // 
+            this.update_Button.AutoSize = true;
+            this.update_Button.Location = new System.Drawing.Point(38, 107);
+            this.update_Button.Name = "update_Button";
+            this.update_Button.Size = new System.Drawing.Size(60, 17);
+            this.update_Button.TabIndex = 5;
+            this.update_Button.TabStop = true;
+            this.update_Button.Text = "Update";
+            this.update_Button.UseVisualStyleBackColor = true;
+            // 
+            // delete_Button
+            // 
+            this.delete_Button.AutoSize = true;
+            this.delete_Button.Location = new System.Drawing.Point(38, 130);
+            this.delete_Button.Name = "delete_Button";
+            this.delete_Button.Size = new System.Drawing.Size(56, 17);
+            this.delete_Button.TabIndex = 6;
+            this.delete_Button.TabStop = true;
+            this.delete_Button.Text = "Delete";
+            this.delete_Button.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Table1",
+            "Table2"});
+            this.listBox1.Location = new System.Drawing.Point(163, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(452, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(452, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(452, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(452, 189);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Operation:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Table:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "<Option 1>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "<Option 2>";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(387, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "<Option 3>";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(387, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "<Option 4>";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "<ah>",
+            "<help>"});
+            this.listBox2.Location = new System.Drawing.Point(38, 242);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 18;
+            // 
+            // EditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.delete_Button);
+            this.Controls.Add(this.update_Button);
+            this.Controls.Add(this.read_Button);
+            this.Controls.Add(this.create_button);
+            this.Controls.Add(this.title_Label);
+            this.Name = "EditForm";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title_Label;
+        private System.Windows.Forms.RadioButton create_button;
+        private System.Windows.Forms.RadioButton read_Button;
+        private System.Windows.Forms.RadioButton update_Button;
+        private System.Windows.Forms.RadioButton delete_Button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
--- Database/Database/Form1.Designer.cs
+++ Database/Database/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.title_Label = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,22 +42,35 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // title_Label
+            // 
+            this.title_Label.AutoSize = true;
+            this.title_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_Label.Location = new System.Drawing.Point(395, 9);
+            this.title_Label.Name = "title_Label";
+            this.title_Label.Size = new System.Drawing.Size(318, 37);
+            this.title_Label.TabIndex = 1;
+            this.title_Label.Text = "Course Registration";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1115, 606);
+            this.Controls.Add(this.title_Label);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label title_Label;
     }
 }
 
