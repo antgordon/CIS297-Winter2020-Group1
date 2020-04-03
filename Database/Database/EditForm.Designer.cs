726b3b4a553f90dcc6332253307de70bb7e7c9fc
AMobOfHamsters <d.tyler.senkbeil@gmail.com>
2020-04-03 16:36:13 -0400

    Revert "."
    This reverts commit 33eebf0fa5c403e71bbb387837a59364e5644569.

--- Database/Database/EditForm.Designer.cs
+++ Database/Database/EditForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Database.CrudTests
+﻿namespace Database
 {
-    partial class EditForm2
+    partial class EditForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,31 +30,28 @@
         {
             this.title_Label = new System.Windows.Forms.Label();
             this.create_Button = new System.Windows.Forms.RadioButton();
+            this.read_Button = new System.Windows.Forms.RadioButton();
             this.update_Button = new System.Windows.Forms.RadioButton();
             this.delete_Button = new System.Windows.Forms.RadioButton();
+            this.table_ListBox = new System.Windows.Forms.ListBox();
             this.option1_TextBox = new System.Windows.Forms.TextBox();
+            this.option2_TextBox = new System.Windows.Forms.TextBox();
             this.option3_TextBox = new System.Windows.Forms.TextBox();
             this.option4_TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.option1_Label = new System.Windows.Forms.Label();
             this.option2_Label = new System.Windows.Forms.Label();
             this.option3_Label = new System.Windows.Forms.Label();
             this.option4_Label = new System.Windows.Forms.Label();
             this.tableContent_ListBox = new System.Windows.Forms.ListBox();
-            this.operationsGroupBox = new System.Windows.Forms.GroupBox();
-            this.submitButton = new System.Windows.Forms.Button();
-            this.table_ListBox = new System.Windows.Forms.ListBox();
-            this.option2_TextBox = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.operationsGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // title_Label
             // 
             this.title_Label.AutoSize = true;
             this.title_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title_Label.Location = new System.Drawing.Point(106, 17);
+            this.title_Label.Location = new System.Drawing.Point(231, 9);
             this.title_Label.Name = "title_Label";
             this.title_Label.Size = new System.Drawing.Size(321, 37);
             this.title_Label.TabIndex = 2;
@@ -63,19 +60,31 @@
             // create_Button
             // 
             this.create_Button.AutoSize = true;
-            this.create_Button.Checked = true;
-            this.create_Button.Location = new System.Drawing.Point(53, 24);
+            this.create_Button.Location = new System.Drawing.Point(38, 61);
             this.create_Button.Name = "create_Button";
             this.create_Button.Size = new System.Drawing.Size(56, 17);
             this.create_Button.TabIndex = 3;
             this.create_Button.TabStop = true;
             this.create_Button.Text = "Create";
             this.create_Button.UseVisualStyleBackColor = true;
+            this.create_Button.Click += new System.EventHandler(this.create_Button_Selected);
+            // 
+            // read_Button
+            // 
+            this.read_Button.AutoSize = true;
+            this.read_Button.Location = new System.Drawing.Point(38, 84);
+            this.read_Button.Name = "read_Button";
+            this.read_Button.Size = new System.Drawing.Size(51, 17);
+            this.read_Button.TabIndex = 4;
+            this.read_Button.TabStop = true;
+            this.read_Button.Text = "Read";
+            this.read_Button.UseVisualStyleBackColor = true;
+            this.read_Button.Click += new System.EventHandler(this.read_Button_Selected);
             // 
             // update_Button
             // 
             this.update_Button.AutoSize = true;
-            this.update_Button.Location = new System.Drawing.Point(53, 46);
+            this.update_Button.Location = new System.Drawing.Point(38, 107);
             this.update_Button.Name = "update_Button";
             this.update_Button.Size = new System.Drawing.Size(60, 17);
             this.update_Button.TabIndex = 5;
@@ -86,7 +95,7 @@
             // delete_Button
             // 
             this.delete_Button.AutoSize = true;
-            this.delete_Button.Location = new System.Drawing.Point(53, 66);
+            this.delete_Button.Location = new System.Drawing.Point(38, 130);
             this.delete_Button.Name = "delete_Button";
             this.delete_Button.Size = new System.Drawing.Size(56, 17);
             this.delete_Button.TabIndex = 6;
@@ -94,37 +103,56 @@
             this.delete_Button.Text = "Delete";
             this.delete_Button.UseVisualStyleBackColor = true;
             // 
+            // table_ListBox
+            // 
+            this.table_ListBox.FormattingEnabled = true;
+            this.table_ListBox.Location = new System.Drawing.Point(163, 61);
+            this.table_ListBox.Name = "table_ListBox";
+            this.table_ListBox.Size = new System.Drawing.Size(120, 95);
+            this.table_ListBox.TabIndex = 7;
+            this.table_ListBox.SelectedValueChanged += new System.EventHandler(this.Table_Selected);
+            // 
             // option1_TextBox
             // 
-            this.option1_TextBox.Enabled = false;
-            this.option1_TextBox.Location = new System.Drawing.Point(500, 66);
+            this.option1_TextBox.Location = new System.Drawing.Point(452, 81);
             this.option1_TextBox.Name = "option1_TextBox";
             this.option1_TextBox.Size = new System.Drawing.Size(100, 20);
             this.option1_TextBox.TabIndex = 8;
-            this.option1_TextBox.Visible = false;
+            // 
+            // option2_TextBox
+            // 
+            this.option2_TextBox.Location = new System.Drawing.Point(452, 117);
+            this.option2_TextBox.Name = "option2_TextBox";
+            this.option2_TextBox.Size = new System.Drawing.Size(100, 20);
+            this.option2_TextBox.TabIndex = 9;
             // 
             // option3_TextBox
             // 
-            this.option3_TextBox.Enabled = false;
-            this.option3_TextBox.Location = new System.Drawing.Point(500, 138);
+            this.option3_TextBox.Location = new System.Drawing.Point(452, 153);
             this.option3_TextBox.Name = "option3_TextBox";
             this.option3_TextBox.Size = new System.Drawing.Size(100, 20);
             this.option3_TextBox.TabIndex = 10;
-            this.option3_TextBox.Visible = false;
             // 
             // option4_TextBox
             // 
-            this.option4_TextBox.Enabled = false;
-            this.option4_TextBox.Location = new System.Drawing.Point(500, 174);
+            this.option4_TextBox.Location = new System.Drawing.Point(452, 189);
             this.option4_TextBox.Name = "option4_TextBox";
             this.option4_TextBox.Size = new System.Drawing.Size(100, 20);
             this.option4_TextBox.TabIndex = 11;
-            this.option4_TextBox.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Operation:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 53);
+            this.label2.Location = new System.Drawing.Point(160, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 13);
             this.label2.TabIndex = 13;
@@ -133,46 +161,38 @@
             // option1_Label
             // 
             this.option1_Label.AutoSize = true;
-            this.option1_Label.Enabled = false;
-            this.option1_Label.Location = new System.Drawing.Point(435, 69);
+            this.option1_Label.Location = new System.Drawing.Point(387, 84);
             this.option1_Label.Name = "option1_Label";
             this.option1_Label.Size = new System.Drawing.Size(59, 13);
             this.option1_Label.TabIndex = 14;
             this.option1_Label.Text = "<Option 1>";
-            this.option1_Label.Visible = false;
             // 
             // option2_Label
             // 
             this.option2_Label.AutoSize = true;
-            this.option2_Label.Enabled = false;
-            this.option2_Label.Location = new System.Drawing.Point(435, 105);
+            this.option2_Label.Location = new System.Drawing.Point(387, 120);
             this.option2_Label.Name = "option2_Label";
             this.option2_Label.Size = new System.Drawing.Size(59, 13);
             this.option2_Label.TabIndex = 15;
             this.option2_Label.Text = "<Option 2>";
-            this.option2_Label.Visible = false;
             // 
             // option3_Label
             // 
             this.option3_Label.AutoSize = true;
-            this.option3_Label.Enabled = false;
-            this.option3_Label.Location = new System.Drawing.Point(435, 142);
+            this.option3_Label.Location = new System.Drawing.Point(387, 157);
             this.option3_Label.Name = "option3_Label";
             this.option3_Label.Size = new System.Drawing.Size(59, 13);
             this.option3_Label.TabIndex = 16;
             this.option3_Label.Text = "<Option 3>";
-            this.option3_Label.Visible = false;
             // 
             // option4_Label
             // 
             this.option4_Label.AutoSize = true;
-            this.option4_Label.Enabled = false;
-            this.option4_Label.Location = new System.Drawing.Point(435, 177);
+            this.option4_Label.Location = new System.Drawing.Point(387, 192);
             this.option4_Label.Name = "option4_Label";
             this.option4_Label.Size = new System.Drawing.Size(59, 13);
             this.option4_Label.TabIndex = 17;
             this.option4_Label.Text = "<Option 4>";
-            this.option4_Label.Visible = false;
             // 
             // tableContent_ListBox
             // 
@@ -182,90 +202,30 @@
             this.tableContent_ListBox.Size = new System.Drawing.Size(710, 199);
             this.tableContent_ListBox.TabIndex = 18;
             // 
-            // operationsGroupBox
-            // 
-            this.operationsGroupBox.Controls.Add(this.submitButton);
-            this.operationsGroupBox.Controls.Add(this.delete_Button);
-            this.operationsGroupBox.Controls.Add(this.update_Button);
-            this.operationsGroupBox.Controls.Add(this.create_Button);
-            this.operationsGroupBox.Location = new System.Drawing.Point(232, 57);
-            this.operationsGroupBox.Name = "operationsGroupBox";
-            this.operationsGroupBox.Size = new System.Drawing.Size(136, 148);
-            this.operationsGroupBox.TabIndex = 19;
-            this.operationsGroupBox.TabStop = false;
-            this.operationsGroupBox.Text = "Operations";
-            // 
-            // submitButton
-            // 
-            this.submitButton.Location = new System.Drawing.Point(30, 106);
-            this.submitButton.Name = "submitButton";
-            this.submitButton.Size = new System.Drawing.Size(82, 29);
-            this.submitButton.TabIndex = 7;
-            this.submitButton.Text = "Submit";
-            this.submitButton.UseVisualStyleBackColor = true;
-            // 
-            // table_ListBox
-            // 
-            this.table_ListBox.FormattingEnabled = true;
-            this.table_ListBox.Location = new System.Drawing.Point(38, 81);
-            this.table_ListBox.Name = "table_ListBox";
-            this.table_ListBox.Size = new System.Drawing.Size(120, 95);
-            this.table_ListBox.TabIndex = 20;
-            this.table_ListBox.SelectedIndexChanged += new System.EventHandler(this.tabl_select);
-            // 
-            // option2_TextBox
-            // 
-            this.option2_TextBox.Enabled = false;
-            this.option2_TextBox.Location = new System.Drawing.Point(500, 102);
-            this.option2_TextBox.Name = "option2_TextBox";
-            this.option2_TextBox.Size = new System.Drawing.Size(100, 20);
-            this.option2_TextBox.TabIndex = 9;
-            this.option2_TextBox.Visible = false;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Enabled = false;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(646, 65);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 23;
-            this.comboBox1.Visible = false;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.Enabled = false;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(646, 102);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 24;
-            this.comboBox2.Visible = false;
-            // 
-            // EditForm2
+            // EditForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.table_ListBox);
-            this.Controls.Add(this.operationsGroupBox);
             this.Controls.Add(this.tableContent_ListBox);
             this.Controls.Add(this.option4_Label);
             this.Controls.Add(this.option3_Label);
             this.Controls.Add(this.option2_Label);
             this.Controls.Add(this.option1_Label);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.option4_TextBox);
             this.Controls.Add(this.option3_TextBox);
             this.Controls.Add(this.option2_TextBox);
             this.Controls.Add(this.option1_TextBox);
+            this.Controls.Add(this.table_ListBox);
+            this.Controls.Add(this.delete_Button);
+            this.Controls.Add(this.update_Button);
+            this.Controls.Add(this.read_Button);
+            this.Controls.Add(this.create_Button);
             this.Controls.Add(this.title_Label);
-            this.Name = "EditForm2";
+            this.Name = "EditForm";
             this.Text = "Form2";
-            this.operationsGroupBox.ResumeLayout(false);
-            this.operationsGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -275,22 +235,20 @@
 
         private System.Windows.Forms.Label title_Label;
         private System.Windows.Forms.RadioButton create_Button;
+        private System.Windows.Forms.RadioButton read_Button;
         private System.Windows.Forms.RadioButton update_Button;
         private System.Windows.Forms.RadioButton delete_Button;
+        private System.Windows.Forms.ListBox table_ListBox;
         private System.Windows.Forms.TextBox option1_TextBox;
+        private System.Windows.Forms.TextBox option2_TextBox;
         private System.Windows.Forms.TextBox option3_TextBox;
         private System.Windows.Forms.TextBox option4_TextBox;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label option1_Label;
         private System.Windows.Forms.Label option2_Label;
         private System.Windows.Forms.Label option3_Label;
         private System.Windows.Forms.Label option4_Label;
         private System.Windows.Forms.ListBox tableContent_ListBox;
-        private System.Windows.Forms.GroupBox operationsGroupBox;
-        private System.Windows.Forms.Button submitButton;
-        private System.Windows.Forms.ListBox table_ListBox;
-        private System.Windows.Forms.TextBox option2_TextBox;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
