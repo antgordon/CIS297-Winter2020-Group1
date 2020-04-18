cf15358b8296c03021ec04c569629d94e942101f
AMobOfHamsters <d.tyler.senkbeil@gmail.com>
2020-04-18 18:34:40 -0400

    Updated GridEntity.cs

--- MineSweeper/MineSweeper/GridEntity.cs
+++ MineSweeper/MineSweeper/GridEntity.cs
@@ -2,17 +2,43 @@
 {
     public class GridEntity
     {
-        public int count { get; set; }
+        public int value { get; set; }  //count of -1 means it is a bomb
         bool flagSet { get; set; }
         bool questionSet { get; set; }
+        bool blankSet { get; set; }
         bool positionRevealed { get; set; }
 
         public GridEntity()
         {
-            count = 0;
+            value = 0;
             flagSet = false;
             questionSet = false;
+            blankSet = true;
             positionRevealed = false;
         }
+
+        public void cycleState()
+        {
+            if(flagSet)
+            {
+                flagSet = false;
+                questionSet = true;
+            }
+            else if(questionSet)
+            {
+                questionSet = false;
+                blankSet = true;
+            }
+            else if(blankSet)
+            {
+                blankSet = false;
+                flagSet = true;
+            }
+        }
+
+        /*public void revealBox()
+        {
+            positionRevealed = true;
+        }*/
     }
 }
