52f9006c3210715660b27f954ef53d517b01a6e7
Kevin Lam <lamkk@umich.edu>
2020-04-18 18:35:46 -0400

    Filling in bombs to GridEntity

--- MineSweeper/MineSweeper/GridEntity.cs
+++ MineSweeper/MineSweeper/GridEntity.cs
@@ -2,14 +2,14 @@
 {
     public class GridEntity
     {
-        public int count { get; set; }
-        bool flagSet { get; set; }
-        bool questionSet { get; set; }
-        bool positionRevealed { get; set; }
+        public int value { get; set; }
+        public bool flagSet { get; set; }
+        public bool questionSet { get; set; }
+        public bool positionRevealed { get; set; }
 
         public GridEntity()
         {
-            count = 0;
+            value = 0;
             flagSet = false;
             questionSet = false;
             positionRevealed = false;
